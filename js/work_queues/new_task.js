d7e7049bb5382ad56301b6d16e8edee2dbd3b29e
ellmille <ellelynn98@gmail.com>
2019-02-27 16:58:18 -0600

    added work queue task

---
+++ js/work_queues/new_task.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+/**
+ * {@link https://www.rabbitmq.com/tutorials/tutorial-two-javascript.html}
+ */
+var amqp = require('amqplib/callback_api');
+var q = 'task_queue';
+/** grab string entered through console or use hello world */
+var msg = process.argv.slice(2).join(' ') || "Hello World!";
+
+/** connect, assert queue and send message to queue*/
+amqp.connect('amqp://localhost', function(err, conn){
+    conn.createChannel(function(err, ch){
+        ch.assertQueue(q, {durable: true});
+        ch.sendToQueue(q, new Buffer(msg), {persistent: true});
+        console.log("[x] Sent '%s'", msg);
+    });
+    //close connection and exit
+    setTimeout(function() { 
+        conn.close(); 
+        process.exit(0); 
+    }, 500);
+});
